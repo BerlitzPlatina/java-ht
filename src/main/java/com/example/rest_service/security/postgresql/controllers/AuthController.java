43bd6344d2c6ab2c9888d82932cd4e56d90cb544
nam.hoanghai <nam.hoanghai@edufit.edu.vn>
2025-02-17 20:52:44 +0700

    save

--- src/main/java/com/example/rest_service/security/postgresql/controllers/AuthController.java
+++ src/main/java/com/example/rest_service/security/postgresql/controllers/AuthController.java
@@ -52,7 +52,7 @@ public class AuthController {
   }
 
   @PostMapping("/signin")
-  public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+  public HtResponseEntity<JwtResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
     Authentication authentication = authenticationManager
         .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
