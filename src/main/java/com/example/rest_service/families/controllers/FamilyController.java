2becc9a11dfe3e906cc929ac321785541df4fcb0
Nam Hoang <nam.hoanghai@educo.edu.vn>
2025-02-20 16:33:07 +0700

    save

--- src/main/java/com/example/rest_service/families/controllers/FamilyController.java
+++ src/main/java/com/example/rest_service/families/controllers/FamilyController.java
@@ -1,6 +1,7 @@
 package com.example.rest_service.families.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/families")
 public class FamilyController {
-
+    @GetMapping("")
+    public String all() {
+        return "Public WTF Content.";
+    }
 }
